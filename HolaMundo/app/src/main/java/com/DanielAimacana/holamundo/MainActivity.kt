8f0ddf0bfba81db5d6ccdcfe81140a573659710f
Daniel Aimacana <55715766+Danieloch14@users.noreply.github.com>
2022-05-17 17:51:14 -0500

    Hola Mundo AS
    Revision de herramientas de Android Studio

---
+++ HolaMundo/app/src/main/java/com/DanielAimacana/holamundo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.DanielAimacana.holamundo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
